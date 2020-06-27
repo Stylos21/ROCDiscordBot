636e05a726d5e6615e48aa842baf4963f2a198b2
Ethan <Ethan@MBA1.local>
2020-06-27 12:34:42 -0400

    Added Settings

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Discord = require('discord.js');
+const Enmap = require("enmap")
 const token = process.env.TOKEN;
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const fs = require("fs");
+client.settings = new Enmap("settings")
+const defaultSettings = {
+    prefix: "w!",
+    welcomeChannel: "725802580428718081",
+    async welcome(user) {
+        const channel = client.channels.cache.get(this.welcomeChannel);
+        if(channel) {
+            message.channel.send()
+        }
+    }
+}
 client.on('ready', () => {
     console.log("Ready");
     client.setActivity("YOU", {type: "WATCHING"})
@@ -19,6 +31,8 @@ client.on('message', (message) => {
         message.channel.send("Hello");
     }
 });
-
+client.on('guildCreate', (guild) => {
+    client.settings.ensure(guild.id, defaultSettings)
+});
 
 client.login(token);
