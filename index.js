4d0045d69bf7753ba018c9d49388ef2ee2a21c5a
Ethan <Ethan@MBA1.local>
2020-06-27 11:50:27 -0400

    Added command file loader to index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const Discord = require('discord.js');
 const token = process.env.TOKEN;
-
 const client = new Discord.Client();
-
+client.commands = new Discord.Collection();
+const fs = require("fs");
 client.on('ready', () => {
     console.log("Ready");
+    client.setActivity("YOU", {type: "WATCHING"})
 });
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 client.on('message', (message) => {
+    if(message.author.bot || !message.guild)
     console.log(message);
     if(message.content == "hello"){
         message.channel.send("Hello");
