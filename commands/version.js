1d8e6d5e9748e5883a8d56066b50ffaca94643c5
Solomon Ucko <solly.ucko@gmail.com>
2020-06-28 16:31:12 -0400

    Add version command

---
+++ commands/version.js
@@ -0,0 +1,14 @@
+const {exec: execSync} = require("child_process");
+const util = require("util");
+
+const exec = util.promisify(execSync);
+
+module.exports = {
+    name: "version",
+    description: "Display the current version of ROCDiscordBot",
+    async execute(client, message, args) {
+        const {channel} = client;
+        let {stdout} = await exec("git rev-parse HEAD");
+        channel.send(stdout);
+    },
+};
