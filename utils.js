391a169b2829c0797f936a97372c866af669985f
Solomon Ucko <solly.ucko@gmail.com>
2020-06-28 18:49:58 -0400

    Use `module.exports` instead of `export`

--- utils.js
+++ utils.js
@@ -1,10 +1,14 @@
-export async function getOrCreateChannelByName(guild, channelName, options) {
-    const channel = guild.channels.cache.find(ch => ch.name === channelName);
-    if (channel) return channel;
-    return guild.channels.create(channelName, options);
-}
-
-export function isOwner(app, user) {
-    const {owner, members} = app;
-    return owner.id === user.id || (members && members.has(user.id));
-}
+module.exports = {
+    async getOrCreateChannelByName(guild, channelName, options) {
+        const channel = guild.channels.cache.find(ch => ch.name === channelName);
+        if (channel) return channel;
+        return guild.channels.create(channelName, options);
+    },
+    isOwner(app, user) {
+        const {owner, members} = app;
+        return owner.id === user.id || (members && members.has(user.id));
+    },
+    wait(millis) {
+        return new Promise((resolve, reject) => setTimeout(resolve, millis));
+    },
+};
