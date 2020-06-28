e52aab3b8378bb19de242828f11538b69f0fed05
Solomon Ucko <solly.ucko@gmail.com>
2020-06-28 15:59:47 -0400

    Clean up index.js

--- index.js
+++ index.js
@@ -1,85 +1,119 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const parser = require("discord-command-parser");
+const fs = require("fs-extra");
+
 const token = process.env.TOKEN;
 const client = new Discord.Client();
+
 client.commands = new Discord.Collection();
-const fs = require("fs-extra");
+
 console.log("running");
+
 client.settings = new Enmap("settings");
+
 const defaultSettings = {
     prefix: "w!",
     welcomeChannel: "welcome",
+    welcomeChannelOptions: {
+        permissionOverwrites: [
+            {
+                id: guild.id,
+                allow: ["VIEW_CHANNEL"],
+                deny: ["SEND_MESSAGES"],
+            },
+        ],
+    },
     welcomeMessage: "Welcome to {{guildname}}, {{user}}",
 };
+
 client.on("ready", () => {
     console.log("Ready");
     client.user.setActivity("YOU", {type: "WATCHING"});
 });
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+
+for (const filename of fs.readdirSync("./commands")) {
+    if (filename.endsWith(".js")) {
+        const command = require(`./commands/${filename}`);
+        client.commands.set(command.name, command);
+    }
 }
+
+async function getOrCreateChannelByName(guild, channelName, options) {
+    const channel = guild.channels.cache.find(ch => ch.name === channelName);
+    if (channel) return channel;
+    return guild.channels.create(channelName, options);
+}
+
 client.on("guildMemberAdd", async (member) => {
-    let message = client.settings.get(member.guild.id).welcomeMessage;
-    const configChannel = client.settings.get(member.guild.id).welcomeChannel;
-    let channel = member.guild.channels.cache.find(ch => ch.name === configChannel);
-    if (!channel) {
-        try {
-            channel = await member.guild.channels.create("welcome", {
-                type: "text", permissionOverwrites: [
-                    {
-                        id: member.guild.id,
-                        allow: ["VIEW_CHANNEL"],
-                        deny: ["SEND_MESSAGES"],
-                    },
-                ],
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    message = message.replace("{{user}}", member.user.tag);
-    message = message.replace("{{guildname}}", member.guild.name);
-    message = message.replace("{{numberusers}}", member.guild.memberCount.toString());
-    channel.send(message);
+    const {guild, user} = member;
+    const {
+        welcomeMessage: messageTemplate,
+        welcomeChannel: channelName,
+        welcomeChannelOptions: options,
+    } = client.settings.get(guild.id);
+
+    getOrCreateChannelByName(guild, channelName, options)
+        .then(
+            channel => {
+                let message = messageTemplate;
+                message = message.replace("{{user}}", user.tag);
+                message = message.replace("{{guildname}}", guild.name);
+                message = message.replace("{{numberusers}}", guild.memberCount.toString());
+                channel.send(message);
+            },
+            error => console.log(error),
+        );
 });
-client.on("message", (message) => {
-    if (message.content === "test") {
-        message.channel.send("hi");
+
+client.on("message", message => {
+    const {content, channel, guild, author} = message;
+
+    if (content === "test") {
+        channel.send("hi");
+        return;
     }
-    if(message.content.startsWith("w!")) {
-        message.channel.send(`Saw message "${message.content}"`);
-    }
-    if (message.author.bot || !message.guild) return;
-    const prefix = client.settings.ensure(message.guild.id, defaultSettings).prefix;
+
+    if (content.startsWith("w!")) channel.send(`Saw message "${content}"`);
+
+    if (author.bot || !guild) return;
+
+    const {prefix} = client.settings.ensure(guild.id, defaultSettings);
+
     const parsed = parser.parse(message, prefix, {
         allowBots: false,
         allowSelf: false,
         ignorePrefixCase: false,
     });
+
     if (!parsed.success) {
         return;
     }
 
-    message.channel.send(`Got command "${message.content}"`);
-    let command = parsed.command;
-    let args = parsed.arguments;
-    let cmd = client.commands.get(command);
-    if (!cmd) return;
-    if (cmd.args === true && !args) {
-        return message.channel.send(`${command.name} needs arguments ${message.author}!`);
+    channel.send(`Got command "${content}"`);
+
+    const {command, arguments} = parsed;
+
+    const handler = client.commands.get(command);
+
+    if (!handler) return;
+
+    if (handler.args === true && !arguments) {
+        return message.reply(`${command.name} needs arguments ${author}!`);
     }
-    if (message.content === "hello") {
-        message.channel.send("Hello");
+
+    if (content === "hello") {
+        message.reply("Hello");
     }
-    cmd.execute(client, message, args).catch((error) => {
+
+    handler.execute(client, message, arguments).catch((error) => {
         console.error(error);
         message.reply("there was an error trying to execute that command!");
     });
 });
+
 client.on("guildCreate", (guild) => {
     client.settings.ensure(guild.id, defaultSettings);
 });
+
 client.login(token);
